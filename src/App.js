37fefaf935d01daea8c8f25f43fa2f7e6811a0de
Mitesh Tank <codewithmitesh@gmail.com>
2021-07-09 12:49:11 +0530

    cleaned for new projects

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 // import logo from './logo.svg';
-import Header from './My-Components/Header';
-import ToDos from "./My-Components/ToDos";
-import ToDoitems from "./My-Components/ToDoitems";
-import Footer from "./My-Components/Footer";
+
 
 
 
 import './App.css';
 
 function App() {
+
+
+
   return (
     <>
-    <Header title = "ToDo-Lister"/>
-    <ToDos/>
-    <ToDoitems/>
-    <Footer/>
+      
     </>
   );
 }
--- src/My-Components/ToDos.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ToDos() {
-    return (
-        <div>
-            ToDoes Works!!!
-        </div>
-    )
-}
